280eeb73c889f51c4fc876c8e0e808d716f80544
maria <serranoortizmaria020@gmail.com>
2023-06-20 17:59:46 +0200

    seguir

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,32 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+//Importamos el modelo userModel
+const User = require("../Model/userModel");
 
-
-// Get, Obtener todos los usuarios
-router.get('/', (req, res) => {
-    res.send('Get, Obtener todos los usuarios');
+//GET, OBTENER TODOS LOS USUARIOS
+router.get("/", async (req, res) => {
+  try {
+    const data = await User.find();
+    res.status(200).json({ status: "succeeded", data, error: null });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "failed", data: null, error: error.message });
+  }
 });
 
-// Get, Obtener un unico usuario por id
-
-router.get('/:id', (req, res) => {
-    res.send(`Get, Obtener un usuario por id ${req.params.id}`);
+// crear un usuario
+router.post("/", async (req, res) => {
+  try {
+    const data = new User({
+      name: req.body.name,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      role: req.body.role,
+      skills: req.body.skills,
+      personality: req.body.personality,
+    });
+    await data.save();
+    res.status(200).json({ status: "succeeded", data, error: null });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "failed", data: null, error: error.message });
+  }
 });
 
-// crear un usuario 
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send(`Post, Crear un usuario ${req.body.name}`);    
+//Borrar un usuario
+router.delete("/:id", (req, res) => {
+  res.send(`DELETE one user ${req.params.id}`);
 });
 
-
-// Borrar un usuario
-router.delete('/:id', (req, res) => {
-    res.send(`Delete, Borrar un usuario ${req.params.id}`);
+//Actualizar un usuario
+router.patch("/:id", (req, res) => {
+  res.send(`PATCH user ${req.params.id}`);
 });
 
-// Actualizar un usuario
-router.patch('/:id', (req, res) => {
-    res.send(`Patch, Actualizar un usuario ${req.params.id}`);
-});
 module.exports = router;
--- Model/userModel.js
+++ Model/userModel.js
@@ -1,26 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-    {
-        name:{
-            type: String,
-            required: true
-        },
-        lastname:{
-            type: String,
-            required: false
-        },
-        email:{
-            type: String,
-            unique: true,
-            required: true
-        },
-        role:{
-            type: String,
-            required: true
-        },
-      
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: Array,
+    required: true,
+    default: [],
+    validate: [
+      (array) =>
+        array.length === 0 ||
+        array.every((element) => {
+          const keys = Object.keys(element);
+          return keys.every(() =>
+            keys.every(
+              () =>
+                typeof element[keys[0]] === "boolean" &&
+                typeof element[keys[1]] === "string"
+            )
+          );
+        }),
+      "No son concretas",
+    ],
+  },
+  personality: {
+    type: Object,
+    required: true,
+    validate: [
+      (obj) =>
+        obj.constructor === Object &&
+        Object.values(obj).every((element) => typeof element === "string"),
+      "Wrong personality object",
+    ],
+  },
+});
 
-    },)
-    
+//El primer parametro es el nombre del modelo
+//El segundo parametro es el esquema
+//EL TERCER PARAMETRO ES EL NOMBRE DE LA COLECCION DENUESTRA BASE DE DATOS
+const User = mongoose.model("User", userSchema, "users");
+
+module.exports = User;
